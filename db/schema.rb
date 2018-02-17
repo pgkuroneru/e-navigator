fc7e1f3ac4de3fb33ee661074fcee2d61cd44b88
pgkuroneru <pgkuroneru@users.noreply.github.com>
2018-02-18 00:32:33 +0900

    初期登録時の年齢表示設定の調整

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20180217043256) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.date "age"
-    t.integer "sex", default: 0
+    t.integer "sex"
     t.string "school_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
