82fdeafea6e5974df561c4213f4a1a58d4e44e4f
pgkuroneru <pgkuroneru@users.noreply.github.com>
2018-02-17 15:45:15 +0900

    devise routes.dbのカスタマイズ

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  devise_for :users
+  devise_for :users, :controllers => {
+      :registrations => 'users/registrations',
+      :sessions => 'users/sessions',
+      :passwords => 'users/passwords'
+  }
+
   resources :users, only: [:index]
+
   devise_scope :user do
     authenticated :user do
       root 'users#index', as: :authenticated_root
+      get "sign_out", :to => "users/sessions#destroy"
     end
 
     unauthenticated do
-      root 'devise/sessions#new', as: :unauthenticated_root
+      root 'users/sessions#new', as: :unauthenticated_root
     end
   end
 
